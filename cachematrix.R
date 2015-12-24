2b0f873e617666ff305567b68ffb1b2f33fa742a
dmnavas <dmnavas@gmail.com>
2015-12-24 17:49:01 +0100

    Reindented lines

--- cachematrix.R
+++ cachematrix.R
@@ -3,32 +3,32 @@
 
 ## Create a matrix with its inverse matrix cached
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Calculate the inverse of a matrix x if the calculations is not cached
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
-  
-  if (!is.null(i)){
-    message("getting cached data")
-    return(i)
-  }
-  
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
+        i <- x$getinverse()
+        
+        if (!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
